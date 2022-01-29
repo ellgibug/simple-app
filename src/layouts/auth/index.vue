93fd66783adea8ead497b649080a5e65b6f02e17
Olga Fomenko <fo@kodix.ru>
2022-01-29 12:22:55 +0300

    form v1

--- src/layouts/auth/index.vue
+++ src/layouts/auth/index.vue
@@ -1,24 +1,12 @@
 <template>
-    <div>
-        <div class="a-page">
-            <div class="a-page__card">
-
-
-<!--                <div class="links">-->
-<!--                    <div v-for="link in links" class="links__item">-->
-<!--                        <router-link :to="{name: link.route}" :key="link.route">-->
-<!--                            {{ link.title }}-->
-<!--                        </router-link>-->
-<!--                    </div>-->
-<!--                </div>-->
-
-                <transition name="fade" mode="out-in">
-                    <router-view/>
-                </transition>
-            </div>
+    <div class="auth-page">
+        <div class="auth-page__card">
+            <transition name="fade" mode="out-in">
+                <router-view/>
+            </transition>
         </div>
     </div>
 </template>
 
-<style src="./styles.scss" lang="scss" scoped></style>
+<style src="./styles.scss" lang="scss"></style>
 <script src="./scripts.js"></script>
--- src/views/auth/login/index.vue
+++ src/views/auth/login/index.vue
@@ -1,37 +1,61 @@
 <template>
-    <div class="a-page-card">
-        <div class="a-page-card__title">Login</div>
-        <div class="a-page-card__form">
-            <div class="form">
-                <div class="form__item">
-                    <label for="email">Email</label>
-                    <input type="text" id="email" v-model="email"/>
-                    <span>Поле обязательно</span>
-                </div>
-                <div class="form__item">
-                    <label for="password">Password</label>
-                    <input type="text" id="password" v-model="password"/>
-                    <span/>
-                </div>
-                <div class="form__item">
-                    <button @click="sendLoginRequest">Login</button>
-                </div>
+<!--    <div class="a-page-card">-->
+<!--        <div class="a-page-card__title">Login</div>-->
+<!--        <div class="a-page-card__form">-->
+<!--            <div class="form">-->
+<!--                <div class="form__item">-->
+<!--                    <label for="email">Email</label>-->
+<!--                    <input type="text" id="email" v-model="email"/>-->
+<!--                    <span>Поле обязательно</span>-->
+<!--                </div>-->
+<!--                <div class="form__item">-->
+<!--                    <label for="password">Password</label>-->
+<!--                    <input type="text" id="password" v-model="password"/>-->
+<!--                    <span/>-->
+<!--                </div>-->
+<!--                <div class="form__item">-->
+<!--                    <button @click="sendLoginRequest">Login</button>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="a-page-card__links links">-->
+<!--            <div class="links__item">-->
+<!--                <router-link :to="{name: 'Register'}">-->
+<!--                    Register-->
+<!--                </router-link>-->
+<!--            </div>-->
+<!--            <div class="links__item">-->
+<!--                <router-link :to="{name: 'ForgotPassword'}">-->
+<!--                    Forgot Password-->
+<!--                </router-link>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+
+    <div class="auth-card">
+
+        <div class="auth-card__header">
+            <div class="auth-card-header">
+                <div class="auth-card-header__item active">Логин</div>
+                <div class="auth-card-header__item">Регистрация</div>
             </div>
         </div>
-        <div class="a-page-card__links links">
-            <div class="links__item">
-                <router-link :to="{name: 'Register'}">
-                    Register
-                </router-link>
-            </div>
-            <div class="links__item">
-                <router-link :to="{name: 'ForgotPassword'}">
-                    Forgot Password
-                </router-link>
+
+        <div class="auth-card__form">
+            форма
+        </div>
+
+        <div class="auth-card__footer">
+            <div class="auth-card-footer">
+                <div class="auth-card-footer__link">
+                    Забыли пароль?
+                </div>
             </div>
         </div>
     </div>
+
 </template>
 
-<style src="./styles.scss" lang="scss" scoped></style>
+<!--<style src="./styles.scss" lang="scss" scoped></style>-->
 <script src="./scripts.js"></script>
